c408ba38f98dde941d50bc9486ee397e5f0fa1fb
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-01-26 23:26:20 +0100

    Added basic home page and style

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,10 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "github-fork-ribbon-css/gh-fork-ribbon.css"
+import './styles/style.css';
 
 // start the Stimulus application
-import './bootstrap';
+// import './bootstrap';
