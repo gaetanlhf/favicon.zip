8fb060827fd3111ec8529f5fcf1754a5aa1c6b36
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-02-18 00:54:19 +0100

    Added symfony/mime, symfony/ux-dropzone and symfony/validator

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
 ];
