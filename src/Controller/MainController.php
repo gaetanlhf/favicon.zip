d10a86983f72b3de95ff3861737ad1dc55933c47
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-02-21 22:38:02 +0100

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\FaviconFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,9 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $faviconForm = $this->createForm(FaviconFormType::class);
         return $this->render('index.html.twig', [
-            'controller_name' => 'MainController',
+            'faviconForm' => $faviconForm->createView()
         ]);
     }
 }
