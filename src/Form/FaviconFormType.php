04aeaeff8178330218a11df486fe77b254b2e076
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-02-21 22:38:09 +0100

    Create FaviconFormType.php

---
+++ src/Form/FaviconFormType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\Dropzone\Form\DropzoneType;
+use Symfony\Component\Validator\Constraints\File;
+
+class FaviconFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('drop', DropzoneType::class, [
+                'label' => false,
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1M',
+                        'mimeTypes' => [
+                            'image/png'
+                        ],
+                        'mimeTypesMessage' => 'The image format must be in .png and must not exceed 1 MB.'
+                    ])
+                ],
+            ])
+            ->add('fortyEightIco', CheckboxType::class, ['label' => 'Include a 48x48 version in the .ico file',  'data' => false, 'label_attr' => ['class' => 'checkbox-switch'], 'attr' => ['autocomplete' => 'off'], 'required' => false])
+            ->add('sixtyFourIco', CheckboxType::class, ['label' => 'Include a 64x64 version in the .ico file', 'data' => false, 'label_attr' => ['class' => 'checkbox-switch'], 'attr' => ['autocomplete' => 'off'], 'required' => false])
+            ->add('android', CheckboxType::class, ['label' => 'Generate manifest.json and Android images', 'data' => false, 'label_attr' => ['class' => 'checkbox-switch'], 'attr' => ['data-bs-toggle' => 'collapse', 'data-bs-target' => '#collapseAndroid', 'autocomplete' => 'off'], 'required' => false])
+            ->add('appName', TextType::class, ['label' => 'Application name', 'attr' => ['placeholder' => 'My app', 'value' => 'My app'], 'row_attr' => [
+                'class' => 'form-floating',
+            ], 'required' => false])
+            ->add('shortAppName', TextType::class, ['label' => 'Short application name', 'attr' => ['placeholder' => 'App', 'value' => 'App'], 'row_attr' => [
+                'class' => 'form-floating',
+            ], 'required' => false])
+            ->add('language', TextType::class, ['label' => 'Language', 'attr' => ['placeholder' => 'en-GB', 'value' => 'en-GB'], 'row_attr' => [
+                'class' => 'form-floating',
+            ], 'required' => false])
+            ->add('startUrl', TextType::class, ['label' => 'Start URL', 'attr' => ['placeholder' => '/', 'value' => '/'], 'row_attr' => [
+                'class' => 'form-floating',
+            ], 'required' => false])
+            ->add('themeColour', TextType::class, ['label' => 'Theme colour', 'attr' => ['placeholder' => '#333', 'value' => '#333'], 'row_attr' => [
+                'class' => 'form-floating',
+            ], 'required' => false])
+            ->add('backgroundColour', TextType::class, ['label' => 'Background colour', 'attr' => ['placeholder' => '#fff', 'value' => '#fff'], 'row_attr' => [
+                'class' => 'form-floating',
+            ], 'required' => false])
+            ->add('display', ChoiceType::class, ['label' => 'Display', 'data' => 'minimal', 'choices'  => [
+                'Fullscreen' => 'fullscreen',
+                'Standalone application' => 'standalone',
+                'Minimal set of UI elements' => 'minimal',
+                'Browser' => 'browser'
+            ], 'placeholder' => false, 'required' => false])
+            ->add('apple', CheckboxType::class, ['label' => 'Generate old apple touch images', 'label_attr' => ['class' => 'checkbox-switch'], 'attr' => ['autocomplete' => 'off'], 'required' => false])
+            ->add('ms', CheckboxType::class, ['label' => 'Generate Windows and IE tile images', 'label_attr' => ['class' => 'checkbox-switch'], 'attr' => ['autocomplete' => 'off'], 'required' => false])
+            ->add('submit', SubmitType::class, ['label' => 'Generate']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
