501f1655f447a111da5100c285e3d35600377591
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-03-29 10:42:06 +0200

    Update GenerateController.php

--- src/Controller/GenerateController.php
+++ src/Controller/GenerateController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Helper\UploaderHelper;
-use App\Helper\GenerateFaviconHelper;
 
 class GenerateController extends AbstractController
 {
@@ -22,14 +21,15 @@ class GenerateController extends AbstractController
         if ($faviconForm->isSubmitted() && $faviconForm->isValid()) {
             $data = $faviconForm->getData();
             $id = $uploaderHelper->uploadFile($this->getParameter('img_temp'), $data['drop']);
-            $favicon = $faviconHelper->generate($this->getParameter('img_temp'), $id, $data["appName"], $data["shortAppName"], $data["language"], $data["startUrl"], $data["themeColour"], $data["backgroundColour"], $data["display"], $data["sixtyFourIco"], $data["fortyEightIco"], $data["apple"], $data["android"], $data["ms"]);
-            if ($favicon == null) {
-                $uploaderHelper->deleteUploadedFile($this->getParameter('img_temp'), $id);
+            if ($id == null) {
                 return $this->redirectToRoute('app_home');
-            } else {
-                $uploaderHelper->deleteUploadedFile($this->getParameter('img_temp'), $id);
-                return $favicon;
             }
+            $favicon = $faviconHelper->generate($this->getParameter('img_temp'), $id, $data["appName"], $data["shortAppName"], $data["language"], $data["startUrl"], $data["themeColour"], $data["backgroundColour"], $data["display"], $data["sixtyFourIco"], $data["fortyEightIco"], $data["apple"], $data["android"], $data["ms"]);
+            $uploaderHelper->deleteUploadedFile($this->getParameter('img_temp'), $id);
+            if ($favicon == null) {
+                return $this->redirectToRoute('app_home');
+            } 
+            return $favicon;
         } else {
             $form_errors = array();
             foreach ($faviconForm->getErrors(true) as $error) {
