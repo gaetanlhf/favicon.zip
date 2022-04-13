63153adf272fc2e5857be9e13e5203d9978f0d69
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-04-13 23:41:06 +0200

    Update UploaderHelper.php

--- src/Helper/UploaderHelper.php
+++ src/Helper/UploaderHelper.php
@@ -25,27 +25,36 @@ class UploaderHelper
     {
         try {
             $this->filesystem->remove($path . $id . ".png");
-            $di = new RecursiveDirectoryIterator($path . $id . "/", FilesystemIterator::SKIP_DOTS);
-            $ri = new RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
-            foreach ($ri as $file) {
-                $file->isDir() ?  $this->filesystem->remove($file) : $this->filesystem->remove($file);
+            if (file_exists($path . $id . "/")) {
+                $di = new RecursiveDirectoryIterator($path . $id . "/", FilesystemIterator::SKIP_DOTS);
+                $ri = new RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
+                foreach ($ri as $file) {
+                    $file->isDir() ?  $this->filesystem->remove($file) : $this->filesystem->remove($file);
+                }
+                $this->filesystem->remove($path . $id . "/");
+                $this->filesystem->remove("favicon-" . $id . ".zip");
             }
-            $this->filesystem->remove($path . $id . "/");
-            $this->filesystem->remove("favicon-". $id . ".zip");
         } catch (IOExceptionInterface $e) {
-            $this->flashBag->add('generator_error', $e->getMessage());
+            $this->flashBag->add("generator_error", $e->getMessage());
         }
     }
 
-    public function uploadFile(string $imgPath, UploadedFile $uploadedfile): string
+    public function uploadFile(string $imgPath, UploadedFile $uploadedfile): ?string
     {
         try {
             $id = uniqid();
-            $newImageFilename = $id . '.png';
+            $newImageFilename = $id . ".png";
             $uploadedfile->move($imgPath, $newImageFilename);
+            $size = getimagesize($imgPath . $newImageFilename);
+            list($height, $width) = $size;
+            if ($height != $width) {
+                $this->deleteUploadedFile($imgPath, $id);
+                $this->flashBag->add("generator_error", "The height and width of the image must be equal.");
+                return null;
+            }
             return $id;
         } catch (IOExceptionInterface $e) {
-            $this->flashBag->add('generator_error', $e->getMessage());
+            $this->flashBag->add("generator_error", $e->getMessage());
             return null;
         }
     }
