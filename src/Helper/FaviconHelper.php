5e887cb98ae728a5b43786229a73f9dd5a474d97
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-04-13 23:40:41 +0200

    Update FaviconHelper.php

--- src/Helper/FaviconHelper.php
+++ src/Helper/FaviconHelper.php
@@ -23,26 +23,26 @@ class FaviconHelper
     public function generate(string $path, string $id, string $appName, string $shortAppName, string $language, string $startUrl, string $themeColour, string $backgoundColour, string $display, bool $sixtyFourIco, bool $fortyEightIco, bool $apple, bool $android, bool $ms, string $tileColour): Response
     {
         $config = array_merge([
-            'filePath'      => $path . $id . ".png",
-            'destPath'      => $path . $id . "/",
-            'appName'       => $appName,
-            'appShortName'  => $shortAppName,
-            'appLanguage'   => $language,
-            'appStartUrl'   => $startUrl,
-            'appThemeColor' => $themeColour,
-            'appBgColor'    => $backgoundColour,
-            'appDisplay'    => $display,
-            'use64Icon'     => $sixtyFourIco,
-            'use48Icon'     => $fortyEightIco,
-            'noOldApple'    => !$apple,
-            'noAndroid'     => !$android,
-            'noMs'          => !$ms
+            "filePath"      => $path . $id . ".png",
+            "destPath"      => $path . $id . "/",
+            "appName"       => $appName,
+            "appShortName"  => $shortAppName,
+            "appLanguage"   => $language,
+            "appStartUrl"   => $startUrl,
+            "appThemeColor" => $themeColour,
+            "appBgColor"    => $backgoundColour,
+            "appDisplay"    => $display,
+            "use64Icon"     => $sixtyFourIco,
+            "use48Icon"     => $fortyEightIco,
+            "noOldApple"    => !$apple,
+            "noAndroid"     => !$android,
+            "noMs"          => !$ms
         ]);
         $generator = new Generator($config);
         try {
             $generator->execute();
             $zip = new ZipArchive();
-            $zipName = 'favicon-' . $id . '.zip';
+            $zipName = "favicon-" . $id . ".zip";
             $zip->open($zipName,  \ZipArchive::CREATE);
             if ($android) {
                 $file = file_get_contents($path . $id . "/favicon/manifest.json");
@@ -101,12 +101,12 @@ class FaviconHelper
             $zip->addFromString(basename($path . $id . "/code.html"), file_get_contents($path . $id . "/code.html"));
             $zip->close();
             $response = new Response(file_get_contents($zipName));
-            $response->headers->set('Content-Type', 'application/zip');
-            $response->headers->set('Content-Disposition', 'attachment;filename="' . $zipName . '"');
-            $response->headers->set('Content-length', filesize($zipName));
+            $response->headers->set("Content-Type", "application/zip");
+            $response->headers->set("Content-Disposition", "attachment;filename=\"$zipName\"");
+            $response->headers->set("Content-length", filesize($zipName));
             return $response;
         } catch (\JDZ\Favicon\Exception\GeneratorException $e) {
-            $this->flashBag->add('generator_error', $e->getMessage());
+            $this->flashBag->add("generator_error", $e->getMessage());
             return new RedirectResponse("/");
         }
     }
