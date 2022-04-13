1e114ac6c77bca7064b0a01ed41d08e89b433440
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-04-13 23:40:49 +0200

    Update GenerateController.php

--- src/Controller/GenerateController.php
+++ src/Controller/GenerateController.php
@@ -9,10 +9,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Helper\UploaderHelper;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class GenerateController extends AbstractController
 {
-    #[Route('/generate', name: 'app_generate')]
+    #[Route("/generate", name: "app_generate")]
     public function index(Request $request, UploaderHelper $uploaderHelper, FaviconHelper $faviconHelper): Response
     {
         $faviconForm = $this->createForm(FaviconFormType::class);
@@ -20,23 +21,26 @@ class GenerateController extends AbstractController
 
         if ($faviconForm->isSubmitted() && $faviconForm->isValid()) {
             $data = $faviconForm->getData();
-            $id = $uploaderHelper->uploadFile($this->getParameter('img_temp'), $data['drop']);
+            $id = $uploaderHelper->uploadFile($this->getParameter("img_temp"), $data["drop"]);
             if ($id == null) {
-                return $this->redirectToRoute('app_home');
+                return $this->redirectToRoute("app_home");
             }
-            $favicon = $faviconHelper->generate($this->getParameter('img_temp'), $id, $data["appName"], $data["shortAppName"], $data["language"], $data["startUrl"], $data["themeColour"], $data["backgroundColour"], $data["display"], $data["sixtyFourIco"], $data["fortyEightIco"], $data["apple"], $data["android"], $data["ms"], $data["tileColour"]);
-            $uploaderHelper->deleteUploadedFile($this->getParameter('img_temp'), $id);
+            $favicon = $faviconHelper->generate($this->getParameter("img_temp"), $id, $data["appName"], $data["shortAppName"], $data["language"], $data["startUrl"], $data["themeColour"], $data["backgroundColour"], $data["display"], $data["sixtyFourIco"], $data["fortyEightIco"], $data["apple"], $data["android"], $data["ms"], $data["tileColour"]);
+            $uploaderHelper->deleteUploadedFile($this->getParameter("img_temp"), $id);
             if ($favicon == null) {
-                return $this->redirectToRoute('app_home');
-            } 
+                return $this->redirectToRoute("app_home");
+            }
+            if ($data["downloadToken"] != null) {
+                $favicon->headers->setCookie(Cookie::create("downloadToken", $data["downloadToken"], strtotime("tomorrow"), "/", null, null, false, false, Cookie::SAMESITE_STRICT));
+            }
             return $favicon;
         } else {
             $form_errors = array();
             foreach ($faviconForm->getErrors(true) as $error) {
                 $form_errors[] = $error->getMessage();
             }
-            $this->addFlash('form_error', $form_errors);
-            return $this->redirectToRoute('app_home');
+            $this->addFlash("form_error", $form_errors);
+            return $this->redirectToRoute("app_home");
         }
     }
 }
