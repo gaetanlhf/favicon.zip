a717ff6e3fb953f0f1cb4933cbacacab8a1abef8
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2022-04-18 18:24:24 +0200

    Update FaviconFormType.php

--- src/Form/FaviconFormType.php
+++ src/Form/FaviconFormType.php
@@ -59,8 +59,8 @@ class FaviconFormType extends AbstractType
                 "Minimal set of UI elements" => "minimal",
                 "Browser" => "browser"
             ], "placeholder" => false, "required" => false])
-            ->add("apple", CheckboxType::class, ["label" => "Generate old apple touch images", "label_attr" => ["class" => "checkbox-switch"], "attr" => ["autocomplete" => "off"], "required" => false])
-            ->add("ms", CheckboxType::class, ["label" => "Generate Windows and IE tile images", "label_attr" => ["class" => "checkbox-switch"], "attr" => ["data-bs-toggle" => "collapse", "data-bs-target" => "#collapseMs", "autocomplete" => "off"], "required" => false])
+            ->add("apple", CheckboxType::class, ["label" => "Generate old Apple touch images", "label_attr" => ["class" => "checkbox-switch"], "attr" => ["autocomplete" => "off"], "required" => false])
+            ->add("ms", CheckboxType::class, ["label" => "Generate Microsoft Windows and IE tile images", "label_attr" => ["class" => "checkbox-switch"], "attr" => ["data-bs-toggle" => "collapse", "data-bs-target" => "#collapseMs", "autocomplete" => "off"], "required" => false])
             ->add("tileColour", TextType::class, ["label" => "Tile colour", "attr" => ["placeholder" => "#333", "value" => "#333"], "row_attr" => [
                 "class" => "form-floating",
             ], "required" => false])
